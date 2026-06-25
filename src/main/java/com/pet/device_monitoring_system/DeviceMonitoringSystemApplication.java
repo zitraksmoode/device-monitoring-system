dd4e628f2a5a6328882d010e56bf20cd8848c3e5
zitraksmoode <Cybindmitrij711@gmail.com>
2026-06-25 17:16:06 +0500

    Initial commit

---
+++ src/main/java/com/pet/device_monitoring_system/DeviceMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pet.device_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMonitoringSystemApplication.class, args);
+	}
+
+}
